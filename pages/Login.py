12cf7c8ae2b93f7826c3d4c3668648012f1cf47d
Jonel Muncan <jonelm1901@gmail.com>
2024-04-14 21:24:30 -0400

    Fixed Registration Widget
    Registration fully working and passwords directly hashed then written to config.yaml

--- pages/Login.py
+++ pages/Login.py
@@ -1,31 +1,21 @@
 import streamlit as st ##1.12.0 originally
-
 import streamlit_authenticator as stauth                                ##user auth. in YAML
 from streamlit_authenticator.utilities.hasher import Hasher
-
-
-import numpy as np                                       
-import pandas as pd
-
 import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
 import s3fs
 from st_files_connection import FilesConnection
-
 import yaml
 from yaml.loader import SafeLoader
 
 ##################################################
-
 st.set_page_config(
     page_title="Login",
     page_icon="🚶",
     layout="centered",
     initial_sidebar_state='auto'
 )
-
 ##################################################
 
 # Define CSS styles
@@ -62,7 +52,6 @@ def local_css(file_name):
 ##st.markdown("Already have an account? [Login here](login)")
 
 ###
-
 hashed_passwords = Hasher(['abc', 'def']).generate()
 
 with open('config.yaml') as file:
@@ -91,35 +80,20 @@ elif st.session_state["authentication_status"] is False:
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
 
-
-##reg doesnt save password, have to fix
+#       @Registreation Widget
 ##considering putting config.yaml into s3 
-##also manually doing registration widget and writing directly to yaml
-##maybe jus implement password chnanger, use abc as default password
 try:
-    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user()
+    (email_of_registered_user, username_of_registered_user, name_of_registered_user) = authenticator.register_user(pre_authorization=False)
     if email_of_registered_user:
-        config['credentials']['usernames'][username_of_registered_user] = {
-                'email': email_of_registered_user,
-                'logged_in': False,                                             # Assuming new users are not logged in by default
-                'name': name_of_registered_user,
-                ##'password': 'abc'                                 # You should hash and store the password securely
-            }
-        activeDates = {
-            "upload_dates": currDate
-        }
-        #post_id = collection.insert_one(post).inserted_id
-        #activeDates = fs.open("posturepriorityawsbucket/"+currUser, mode='rb')
         st.success('User registered successfully')
         with open('config.yaml', 'w') as file:
             yaml.dump(config, file, default_flow_style=False)
-
 except Exception as e:
-    st.error(e)
+        st.error(e)
 
 
-
-if st.session_state["authentication_status"]:
+#       @Password Reset Widget
+if st.session_state["authentication_status"] and st.button("Reset Password?"):
     try:
         if authenticator.reset_password(st.session_state["username"]):
             st.success('Password modified successfully')
